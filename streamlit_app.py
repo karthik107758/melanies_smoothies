501f9a266b6d99813ebf1025452c74180e898042
KarthikPolisetty <polisetty.kartheek@gmail.com>
2025-10-27 15:52:58 -0400

    Update streamlit_app.py
    rearranged the code into forloop to display the data for selected fruits from dropdown

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,8 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -49,6 +51,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered,{name_on_order}!", icon="✅")
        
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
